9836cf870c69ea54147f9163051c90a9da578667
Deepak Gupta <deepakg6797@gmail.com>
2024-05-12 17:05:07 +0545

    react ci cd

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-serve -s . &
+# Find the PID listening on port 3000
+PID=$(ss -tulpn | grep ':3000' | awk '{print $NF}' | cut -d '=' -f 2)
+
+if [ -n "$PID" ]; then
+    echo "Process with PID $PID found running on port 3000. Killing..."
+    kill $PID
+else
+    echo "No process found running on port 3000."
+fi
+
+nohup serve -s . > /dev/null 2>&1 &
